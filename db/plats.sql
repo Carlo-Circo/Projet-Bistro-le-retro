5e09ad28c5f060c784fce6069896d3a45353a525
Carlo Circo <Carlo.Circo@outlook.fr>
2026-05-11 12:30:43 +0200

    fix: fix name length in plats.sql

--- db/plats.sql
+++ db/plats.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS plats ( 
     id INT AUTO_INCREMENT PRIMARY KEY, 
-    nom VARCHAR(30) NOT NULL, 
+    nom VARCHAR(100) NOT NULL, 
     description text NOT NULL, 
     prix DECIMAL(10, 2) NOT NULL, 
     categorie ENUM('entree', 'plat', 'dessert') NOT NULL, 
